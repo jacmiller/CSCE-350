ea69b879cdbb4ab9c5dafc47e106f91f0aa1b2c2
Christopher 'Jacob' Miller <cjmiller1337@gmail.com>
2018-09-06 14:44:26 -0400

    Added Hoare Partition Quicksort

---
+++ Project1/build/CMakeFiles/runUnitTests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jacob/CSCE350/program-1-jacmiller/src/targetgtest.cpp" "/home/jacob/CSCE350/program-1-jacmiller/build/CMakeFiles/runUnitTests.dir/src/targetgtest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../googletest/include"
+  "../googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jacob/CSCE350/program-1-jacmiller/build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/home/jacob/CSCE350/program-1-jacmiller/build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Project1/src/quicksort.h
@@ -0,0 +1,76 @@
+/**
+ * Conceived by: Christopher "Jacob" Miller
+ * CSCE 350 - O'Reilly
+ * 9/6/18
+ */
+
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+#include <algorithm>
+
+using std::swap;
+
+//note returns INDEX of median
+//does NOT move the median into the pivot position
+template<typename T>
+inline
+int medianOf3(T A[], int l, int r) {
+    //this is overcommented... also, try and avoid using pointers
+    T *a = A + l;//array name is just pointer to 1st (0 index) elem., + l shifts l*(T size)
+    T *b = A + l + (r - l) / 2;//middle item... int division rounds down
+    T *c = A + r;
+
+    //when a is a pointer, *a is the dereference operator (gives value a points to)
+    T *m;
+    if (*a < *b) {
+        if (*b < *c) m = b;
+        else if (*c < *a) m = a;
+        else m = c;
+    } else { //b <=a
+        if (*a < *c) m = a;
+        else if (*c < *b) m = b;
+        else m = c;
+    }
+    return m - A; //m-A is the number of elements from A[0]
+
+}
+
+//remember: l and r are INLCUSIVE (just like Lomuto)
+//make sure to call medianOf3 on the subarray before partitioning
+template<typename T>
+int hoarePartition(T A[], int l, int r) {
+
+    T p = A[medianOf3(A, l, r)]; //Creates a pivot point, using median of 3 function
+
+
+    int i = l-1; //Setting left index
+    int j = r + 1; //Setting right index
+
+    while (true) {
+        do i=i+1; //Increment i reaching pivot point
+        while ( A[i] < p );
+
+        do j = j - 1; //Decrement j from pivot point
+        while (A[j] > p);
+
+        if (i >= j) //Halting condition
+        {
+            return j;
+        }
+        swap(A[i], A[j]);
+    }
+    return 0;
+}
+
+template<typename T>
+void quicksort(T A[], int l, int r) {
+
+    if (l < r) { // Comparison for left/right pivot
+        T s = hoarePartition(A,l,r); //Creating new pivot using partition function
+        quicksort(A,l,s); //Recursive call with left pivot point
+        quicksort(A,s+1,r); //Recursive call on right half pivot point
+    }
+}
+
+#endif
