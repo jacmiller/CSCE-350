6d2320d18523fa67427e584bb77d8c474c27f807
Christopher 'Jacob' Miller <cjmiller1337@gmail.com>
2018-09-06 17:06:01 -0400

    Final Version of Hoare Quicksort

--- Project1/src/quicksort.h
+++ Project1/src/quicksort.h
@@ -40,22 +40,23 @@ int medianOf3(T A[], int l, int r) {
 //make sure to call medianOf3 on the subarray before partitioning
 template<typename T>
 int hoarePartition(T A[], int l, int r) {
-
+    int pind = medianOf3(A, l, r);
     T p = A[medianOf3(A, l, r)]; //Creates a pivot point, using median of 3 function
+    swap(A[pind],A[l]);
 
-
-    int i = l-1; //Setting left index
+    int i = l; //Setting left index
     int j = r + 1; //Setting right index
 
     while (true) {
         do i=i+1; //Increment i reaching pivot point
-        while ( A[i] < p );
+        while ( A[i] < p);
 
         do j = j - 1; //Decrement j from pivot point
         while (A[j] > p);
 
         if (i >= j) //Halting condition
         {
+	    swap(A[l],A[j]);
             return j;
         }
         swap(A[i], A[j]);
@@ -68,7 +69,7 @@ void quicksort(T A[], int l, int r) {
 
     if (l < r) { // Comparison for left/right pivot
         T s = hoarePartition(A,l,r); //Creating new pivot using partition function
-        quicksort(A,l,s); //Recursive call with left pivot point
+        quicksort(A,l,s-1); //Recursive call with left pivot point
         quicksort(A,s+1,r); //Recursive call on right half pivot point
     }
 }
